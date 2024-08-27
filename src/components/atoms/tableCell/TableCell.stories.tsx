03b88eb3e116ee5f6f9a5c0d1c2bdff9bee7b9f8
Riyasfrd <riyasf@alliontechnologies.com>
2024-08-27 09:05:30 +0530

    Table Cell

---
+++ src/components/atoms/tableCell/TableCell.stories.tsx
@@ -0,0 +1,36 @@
+import { TableCells } from './TableCells';
+
+export default {
+  title: 'Components/Atoms/TableCell',
+  component: TableCells,
+  tags: ['autodocs'],
+  argstype: {
+    
+    value: {
+      description: 'Value to be displayed in the cell',
+      control: 'text',
+    },
+    format: {
+      description: 'Function to format the value',
+      control: 'text',
+    },
+    isImage: {
+      description: 'Whether the value is an image or not',
+      control: 'boolean',
+    },
+    columnIndex: {
+      description: 'Index of the column',
+      control: 'number',
+    },
+  },
+};
+const Template = {
+    args: {
+        value: 'Paper',
+        format: (value: any) => value,
+        isImage: false,
+        columnIndex: 3,
+    },
+};
+
+export const Default = Template;
