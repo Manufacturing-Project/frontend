e9a199b2e08b03c73da33304c4f6d7b24c1fd752
Tharuallion <tharushik@alliontechnologies.com>
2024-10-07 15:01:01 +0530

    toaster-fix

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -3,7 +3,5 @@ export { AddRawMaterial } from './addRawMaterial/AddRawMaterial';
 export { MenuBar } from './menuBar/MenuBar';
 export {Itembox} from './itembox/Itembox';
 export {SearchBar} from './searchbar/Searchbar';
-
-
 export { Logo } from './logo/Logo';
 export { TableCells } from './tableCell/TableCells';
