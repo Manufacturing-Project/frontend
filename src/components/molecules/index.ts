1b53e70264c1b27754d7fb71e82689cfca65f303
kavindyag <kavindyag@alliontechnologies.com>
2024-10-07 21:37:16 +0530

    chane the AddRawmaterial form styles

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,7 +1,10 @@
-
+export {InputTextField} from './inputField/inputTextField/InputTextField';
+export {InputTextArea} from './inputField/inputTextArea/InputTextArea';
+export {InputSelectField} from './inputField/inputSelectField/InputSelectField';
 export { AddRawMaterial } from './addRawMaterial/AddRawMaterial';
 export { MenuBar } from './menuBar/MenuBar';
 export {Itembox} from './itembox/Itembox';
 export {SearchBar} from './searchbar/Searchbar';
 export { Logo } from './logo/Logo';
 export { TableCells } from './tableCell/TableCells';
+
