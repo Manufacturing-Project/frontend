c9b760c1ce465d115d3a91eaf74aee339b582332
Tharuallion <tharushik@alliontechnologies.com>
2024-10-07 14:55:55 +0530

    hii

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -7,5 +7,3 @@ export {SearchBar} from './searchbar/Searchbar';
 
 export { Logo } from './logo/Logo';
 export { TableCells } from './tableCell/TableCells';
-export { SearchBar } from './searchBar/SearchBar';
-export { Itembox } from './itembox/Itembox';
