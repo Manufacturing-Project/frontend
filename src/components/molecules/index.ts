5661de288f80110a13e339f5091630ffa7761618
sajad-allion <sajadd@alliontechnologies.com>
2024-10-08 09:08:50 +0530

    gg

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,6 +1,6 @@
 
 export { AddRawMaterial } from './addRawMaterial/AddRawMaterial';
-export { MenuBar } from './menuBar/MenuBar';
+export { MenuBar } from './MenuBar/MenuBar';
 export {Itembox} from './itembox/Itembox';
 export {SearchBar} from './searchbar/Searchbar';
 export { Logo } from './logo/Logo';
